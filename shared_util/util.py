23255703f5ac597e9ca477eb10f6fb602cdf6a8d
Zhimao Lin <zhimaolin@outlook.com>
2022-04-13 21:45:33 +0800

    Added draw_line_chart function

--- shared_util/util.py
+++ shared_util/util.py
@@ -1,4 +1,5 @@
 import pandas as pd
+import matplotlib.pyplot as plt
 
 
 # Write a row to a file. The row has to be a tuple of strings
@@ -17,4 +18,30 @@ def split_dataset(data_summary_path, train_data_csv_path, test_data_csv_path, ra
 
     train_df.to_csv(train_data_csv_path, index=False)
     test_df.to_csv(test_data_csv_path, index=False)
+
+
+
+def draw_line_chart(x_list, y_list, title, x_label, y_label, save_path, is_show=False):
+    plt.figure()
+    plt.plot(x_list, y_list)
+    plt.title(title)
+    plt.xlabel(x_label)
+    plt.ylabel(y_label)
+    # plt.xticks(fontsize=14)
+    # plt.yticks(fontsize=14)
+    plt.tight_layout()
+    if is_show:
+        plt.show()
+    plt.savefig(save_path)
+    # plt.close()  
+
+
+# y_list = [10, 9,8,7,6,5,4,3,2,1]
+# x_list = range(len(y_list))
+# title = "Title"
+# x_label = "x_lable"
+# y_label = "y_lable"
+# save_path = "foo.png"
+# is_show = True
+# draw_train_loss_chart(x_list, y_list, title, x_label, y_label, save_path, is_show=False)
 
