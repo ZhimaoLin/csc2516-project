091be45e985c0fab650f63e10df3f08849fc90f2
Zhimao Lin <zhimaolin@outlook.com>
2022-04-13 21:02:13 +0800

    Update util.py

--- shared_util/util.py
+++ shared_util/util.py
@@ -1,6 +1,13 @@
 import pandas as pd
 
 
+# Write a row to a file. The row has to be a tuple of strings
+# Input: "./data_summary.csv", ("person_name", "video_name", "frame_number", "pspi_score", "image_path"), "w"
+# output: None
+def write_row_to_file(file_path, row, mode="a"):
+    with open(file_path, mode) as f:
+        f.write(",".join(row) + "\n")
+
 
 def split_dataset(data_summary_path, train_data_csv_path, test_data_csv_path, random_state, train_fraction=0.8):
     df = pd.read_csv(data_summary_path)
